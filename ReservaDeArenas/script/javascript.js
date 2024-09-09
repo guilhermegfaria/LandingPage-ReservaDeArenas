2a34248972b25a0d5bc2210b60d2e5ee02e453cf
guilhermegfaria <133894608+guilhermegfaria@users.noreply.github.com>
2024-09-09 14:24:10 -0300

    Delete ReservaDeArenas directory

--- ReservaDeArenas/script/javascript.js
+++
@@ -1,6 +0,0 @@
-// JavaScript for handling form submission
-document.getElementById('leadForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Obrigado pelo contato! Em breve entraremos em contato.');
-    this.reset();
-});
